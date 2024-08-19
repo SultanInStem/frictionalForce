ed6e3ca4b10ec294e1efae409c3e93a0db64f370
SultanInStem <sultaninstem004@gmail.com>
2024-08-19 14:44:51 -0700

    minor

--- include/Incline.hpp
+++ include/Incline.hpp
@@ -6,6 +6,9 @@
 class Incline{
     private:
         sf::CircleShape triangle;
+        float static_Coefficient; 
+        float kinetic_Coefficient;
+        float angle;
     public: 
         Incline(float angle, sf::Vector2f position, sf::Vector2f size){
             triangle.setPointCount(3); 
