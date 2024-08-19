efed7b597e020fb785ed5aa222c2069a363006e1
SultanInStem <sultaninstem004@gmail.com>
2024-08-18 19:53:29 -0700

    canvas

--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -1,17 +1,22 @@
 #include "Canvas.hpp"
 
 Canvas::Canvas(int width, int height){
-    
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;  
+    window.create(sf::VideoMode(width,height), "Friction Force", sf::Style::Titlebar | sf::Style::Close, settings); 
 }; 
-Canvas::~Canvas(){
-
-}
+Canvas::~Canvas(){}
 
 void Canvas::run(){
 
 }
 void Canvas::handleEvents(){
-
+    sf::Event event; 
+    while(window.pollEvent(event)){
+        if(event.type == sf::Event::Closed){
+            window.close();
+        }
+    }
 }
 void Canvas::update(){
 
